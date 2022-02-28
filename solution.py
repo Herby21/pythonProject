6d2af7dc2cd130d16b659ffd359c7e2d1881961a
Rinat <galeev_05@list.ru>
2022-02-28 20:02:02 +0300

    Initial commit

---
+++ solution.py
@@ -0,0 +1,53 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def stat():
+    return 'Миссия Колонизация Марса'
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion_image')
+def bootstrap():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <link rel="stylesheet" href="static/css/style.css">
+                    <title>Колонизация</title>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.png')}">
+                    <div class="alert alert-dark" role="alert">
+                      Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      И начнём с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Присоединяйся!
+                    </div>
+                  </body>
+                </html>'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
